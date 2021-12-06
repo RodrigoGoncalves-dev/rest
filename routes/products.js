aed46870e70b9eedb1c1de547d63da21eefbed2e
Rodrigo Gonçalves <rodrigo.goncalves2001r@gmail.com>
2021-12-06 02:13:52 -0300

    upload of the image

--- routes/products.js
+++ routes/products.js
@@ -2,10 +2,37 @@ const { Router } = require("express");
 const router = new Router();
 const productController = require("../controllers/ProductController");
 
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads")
+  },
+  filename: function(req, file, cb) {
+    cb(null, new Date().toISOString().replace(/:/g, '-') + "-" + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+}
+
+const upload = multer({ 
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+  fileFilter: fileFilter,
+});
+
 router.get("/", productController.index);
-router.post("/", productController.store);
+router.post("/", upload.single("product_image"),productController.store);
 router.get("/:id", productController.show);
-router.patch("/:id", productController.update);
+router.patch("/:id", upload.single("product_image"),productController.update);
 router.delete("/:id", productController.delete);
 
 module.exports = router;
