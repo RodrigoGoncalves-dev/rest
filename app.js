ecbd23cb2a0844e5eb808ebdaf669c898f702302
Rodrigo Gonçalves <rodrigo.goncalves2001r@gmail.com>
2021-12-06 02:14:48 -0300

    added route user and route upload

--- app.js
+++ app.js
@@ -5,16 +5,19 @@ const cors = require("cors");
 
 const routeProduct = require("./routes/products");
 const routeDemand = require("./routes/demands");
+const routeUser = require("./routes/users");
 const bodyParser = require("body-parser");
 
+app.use(cors("*"));
 app.use(morgan("dev"));
+
+app.use("/uploads", express.static("uploads"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(cors("*"));
-
 app.use("/products", routeProduct);
 app.use("/demands", routeDemand);
+app.use("/users", routeUser);
 
 app.use((req, res, next) => {
   const erro = new Error("Não encontrada");
