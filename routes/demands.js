eac9b65041fe8beb304c44638b523e774b401f09
Rodrigo Gonçalves <rodrigo.goncalves2001r@gmail.com>
2021-12-08 00:41:29 -0300

    add: middleware client

--- routes/demands.js
+++ routes/demands.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 const router = new Router();
 const demandController = require("../controllers/DemandController");
+const client = require("../middleware/middlewareClient");
 
-router.get("/", demandController.index);
-router.post("/", demandController.store);
-router.get("/:id", demandController.show);
-router.patch("/:id", demandController.update);
-router.delete("/:id", demandController.delete);
+router.get("/", client, demandController.index);
+router.post("/", client, demandController.store);
+router.get("/:id", client, demandController.show);
+router.patch("/:id", client, demandController.update);
+router.delete("/:id", client,demandController.delete);
 
 module.exports = router;
