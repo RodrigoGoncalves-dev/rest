4216710261d8ecfacd0ce4c90300fcd238954eca
rodrigo-dev <rodrigo.goncalves2001r@gmail.com>
2022-05-20 17:44:17 -0300

    created class with some httpResponses

---
+++ src/handlers/http-response.js
@@ -0,0 +1,84 @@
+const ConflictError = require('./confict-error');
+const ServerError = require('./server-error');
+const UnauthorizedError = require('./unauthorized-error');
+
+module.exports = class HttpResponse {
+  static badRequest(res, error) {
+    return res.status(400).send({
+      statusCode: 400,
+      body: {
+        message: error.message,
+        error: error.name,
+      }
+    });
+  };
+
+  static notFound(res, message) {
+    return res.status(404).send({
+      statusCode: 404,
+      message: message,
+    })
+  }
+
+  static conflictError(res, errMessage) {
+    return res.status(409).send({
+      statusCode: 409,
+      body: {
+        message: new ConflictError(errMessage).message,
+        error: new ConflictError(errMessage).name
+      }
+    });
+  };
+
+  static serverError(res) {
+    return res.status(500).send({
+      statusCode: 500,
+      body: {
+        message: new ServerError().message,
+        error: new ServerError().name,
+      },
+    });
+  };
+
+  static unauthorizedError(res, message) {
+    return res.status(401).send({
+      statusCode: 401,
+      body: {
+        message: new UnauthorizedError(message).message,
+        error: new UnauthorizedError(message).name
+      },
+    });
+  };
+
+  static ok(res, data, token) {
+    if (token) {
+      return res.status(200).send({
+        statusCode: 200,
+        body: {
+          data: {
+            message: data,
+            token: token
+          },
+        },
+      })
+    }
+    return res.status(200).send({
+      statusCode: 200,
+      body: data,
+    })
+  }
+
+  static created(res, data) {
+    return res.status(201).send({
+      statusCode: 201,
+      body: data,
+    });
+  };
+
+  static deleted(res, data) {
+    return res.status(202).send({
+      statusCode: 202,
+      body: data,
+    });
+  };
+}
